140bd7fbadac2c04f3cbf4efaf7e8e20d766ca90
Anonymous User <anonymous-user@some-email.com>
2020-05-13 17:01:55 +0200

    Update server.ts

--- src/examples/server.ts
+++ src/examples/server.ts
@@ -5,16 +5,19 @@ import LinkedInProfileScraper from '../index';
 
 const app = express();
 
+// Setup environment variables to fill the sessionCookieValue
 const scraper = new LinkedInProfileScraper({
   sessionCookieValue: `${process.env.LINKEDIN_SESSION_COOKIE_VALUE}`,
   keepAlive: false
 })
 
+// Usage: http://localhost:3000/?url=https://www.linkedin.com/in/jvandenaardweg/
 app.get('/', async (req, res) => {
   const urlToScrape = req.query.url as string;
 
   const result = await scraper.run(urlToScrape)
-  res.json(result)
+  
+  return res.json(result)
 })
 
 app.listen(process.env.PORT || 3000)
