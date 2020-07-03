867e8c9b1570a890dbfb175ba88fa0529df496f5
Anonymous User <anonymous-user@some-email.com>
2020-07-03 11:40:31 +0200

    Added some more unit tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -10,7 +10,7 @@ const defaultOptions = {
 
 describe('LinkedInProfileScraper', () => {
 
-  describe('constructor', () => {
+  describe('options', () => {
     it('should set the default options', () => {
       const scraper = new LinkedInProfileScraper({
         sessionCookieValue: 'test'
@@ -30,8 +30,19 @@ describe('LinkedInProfileScraper', () => {
         headless: false
       })
     });
+
+    it('should error when headless is not a boolean', () => {
+      try {
+        new LinkedInProfileScraper({
+          headless: 'something' as any,
+          sessionCookieValue: 'test'
+        });
+      } catch (err) {
+        expect(err.message).toBe('Error during setup. Option "headless" needs to be a boolean.');
+      }
+    });
     
-    it('should set the headless option', () => {
+    it('should set the keepAlive option', () => {
       const scraper = new LinkedInProfileScraper({
         keepAlive: true,
         sessionCookieValue: 'test'
@@ -41,6 +52,25 @@ describe('LinkedInProfileScraper', () => {
         ...defaultOptions,
         keepAlive: true,
       })
+    });
+
+    it('should error when keepAlive is not a boolean', () => {
+      try {
+        new LinkedInProfileScraper({
+          keepAlive: 'something' as any,
+          sessionCookieValue: 'test'
+        });
+      } catch (err) {
+        expect(err.message).toBe('Error during setup. Option "keepAlive" needs to be a boolean.');
+      }
+    });
+    
+    it('should have the sessionCookieValue option', () => {
+      try {
+        new LinkedInProfileScraper({} as any);
+      } catch (err) {
+        expect(err.message).toBe('Error during setup. Option "sessionCookieValue" is required.');
+      }
     });
     
     it('should set the sessionCookieValue option', () => {
@@ -52,6 +82,16 @@ describe('LinkedInProfileScraper', () => {
         ...defaultOptions,
         sessionCookieValue: 'test-again',
       })
+    });
+
+    it('should error when sessionCookieValue is not a string', () => {
+      try {
+        new LinkedInProfileScraper({
+          sessionCookieValue: 123 as any
+        });
+      } catch (err) {
+        expect(err.message).toBe('Error during setup. Option "sessionCookieValue" needs to be a string.');
+      }
     });
     
     it('should set the timeout option', () => {
@@ -65,6 +105,17 @@ describe('LinkedInProfileScraper', () => {
         timeout: 30000,
       })
     });
+
+    it('should error when timeout is not a number', () => {
+      try {
+        new LinkedInProfileScraper({
+          timeout: '1000' as any,
+          sessionCookieValue: 'test'
+        });
+      } catch (err) {
+        expect(err.message).toBe('Error during setup. Option "timeout" needs to be a number.');
+      }
+    });
     
     it('should set the userAgent option', () => {
       const scraper = new LinkedInProfileScraper({
@@ -76,6 +127,17 @@ describe('LinkedInProfileScraper', () => {
         ...defaultOptions,
         userAgent: 'test agent',
       })
+    });
+    
+    it('should error when userAgent is not a string', () => {
+      try {
+        new LinkedInProfileScraper({
+          userAgent: 123 as any,
+          sessionCookieValue: 'test'
+        });
+      } catch (err) {
+        expect(err.message).toBe('Error during setup. Option "userAgent" needs to be a string.');
+      }
     });
   })
 })
--- src/typings/global.ts
+++
@@ -1,5 +0,0 @@
-interface Window {
-  // TODO: use method interface from utils
-  getCleanText: any,
-  getLocationFromText: any
-}
